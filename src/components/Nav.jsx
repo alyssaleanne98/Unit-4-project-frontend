aab5a7109f9347f36aec7bf1306a1ada2926067a
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-02-18 23:53:26 -0800

    styling added nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Nav.css";
+import { Link } from 'react-scroll';
 
-function Nav () {
+const Nav = () => {
     return (
-        <div className="Nav">Navigation Bar</div>
+        <div className="container">
+            <div className="app-title">
+                <h1>Headspace</h1>
+                <h3>Better brain health</h3>
+            </div>
+
+
+            <Link activeClass="active" to="about-section" spy={true} duration={400}>
+                ABOUT 
+            </Link>
+
+            <Link activeClass="active" to="cards-section" spy={true} duration={400}>
+                 HEALTH
+            </Link>
+        </div>
+
+
 
     )
 }
 
 export default Nav;
+
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Section.css"
 
+
 const Section = () => {
     return (
         <main>
-            <section className='about'>
+
+            <section className='about' id='about-section'
+                data-scroll //This attribute makes this section an independent scrollable container
+                data-scroll-speed="9"
+                data-scroll-section
+                >
                 <h1>This is the about section</h1>
             </section>
 
-            <section className='flashcards'>
+            <section className='cards' id='cards-section'>
                 <h1>This is the flashcard section</h1>
             </section>
 
