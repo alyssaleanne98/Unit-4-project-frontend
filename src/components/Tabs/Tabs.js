f3dcae749a3ae2d574919e3fe9a6e2df84be1617
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-03-28 16:56:39 -0700

    tab table in progress

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types'; // mechanism to ensure to use correct data type and pass the right data
-import Tab from './Tab';
+// import Tab from './Tab';
 // Imports are needed to keep track of which tab is active, display a list of tabs, and the content for the active tabs.
 
 
@@ -18,7 +18,7 @@ class Tab extends Component { //extend keywork allows the component to access al
     }
 
     onClickTabItem = (tab) => {
-        this.setState({activeTab:tab});
+        this.setState({ activeTab: tab });
     }
 }
 // The initial state is added for the active tab and will start at 0 in the array of tabs I am creating. 
@@ -26,42 +26,45 @@ class Tab extends Component { //extend keywork allows the component to access al
 
 // Now, I can add render function
 
-render () {
+render() {
     const {
-        onClickTabItem,
-        props: {
-            children,
-        },
-        state: {
-            activeTab,
-        }
+      onClickTabItem,
+      props: {
+        children,
+      },
+      state: {
+        activeTab,
+      }
     } = this;
 
     return (
-        <div className='all-tabs'>
-            <ol className='tab-list'>
-                {children.map((child)=> {
-                    const { label } = child.props;
+      <div className="tabs">
+        <ol className="tab-list">
+          {children.map((child) => {
+            const { label } = child.props;
 
-                    return (
-                        <Tab 
-                        activeTab={activeTab}
-                        key={label}
-                        label= {label}
-                        onClick={onClickTabItem}
-                        />
-                    );
-                })}
-
-            </ol>
-            <div className="tab-content">
-                {children.map((child) => {
-                    if (child.props.label !== activeTabe) return undefined;
-                    return child.props.children;
-                })}
-            </div>
+            return (
+              <Tab
+                activeTab={activeTab}
+                key={label}
+                label={label}
+                onClick={onClickTabItem}
+              />
+            );
+          })}
+        </ol>
+        <div className="tab-content">
+          {children.map((child) => {
+            if (child.props.label !== activeTab) return undefined;
+            return child.props.children;
+          })}
         </div>
-    )
-    }
+      </div>
+    );
+  }
+}
 
-    export default Tabs;
+export default Tabs;
+
+    // the component keeps track of which tab is active, content for each tab, and displays a list of tabs. 
+    // the tabs component uses the next component you will create called "Tab"
