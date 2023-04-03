dacedbba45e010e4edb36466a3498a44f0a7c98e
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 18:27:28 -0700

    tabs component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Nav from "./components/Nav";
 import Main from "./components/Main";
-// import { Routes, Route } from "react-router-dom"
+import Tabs from "./components/Tabs/Tabs"
+// import Tab from "./components/Tabs/Tabs"
+import React, { useState} from 'react';
 // // import Create from "./pages/Create/Create.jsx";
 // import Show from "./pages/Show/Show.jsx";
 // import Index from "./pages/Index/Index.jsx";
@@ -15,11 +17,13 @@ console.log("hi")
   return (
     <div className="App">
       <Nav />
+      <Tabs />
       {/* <Section /> */}
       <Main />
     </div>
   );
 };
+
 
 
 
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,70 +1,11 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types'; // mechanism to ensure to use correct data type and pass the right data
-// import Tab from './Tab';
-// Imports are needed to keep track of which tab is active, display a list of tabs, and the content for the active tabs.
+import React from "react";
 
-
-class Tab extends Component { //extend keywork allows the component to access all of the components properties, including function
-    static propTypes = {
-        children: PropTypes.instanceOf(Array).isRequired,
-    }
-
-    constructor(props) { // In Reactm constructors are mainly used for two purposes: Its used for initializing the local state of the component by assigning an object to this. state 
-        super(props); // super(props) pass the props to the parent contructor
-
-        this.state = {
-            activeTab: this.props.children[0].props.label,
-        };
-    }
-
-    onClickTabItem = (tab) => {
-        this.setState({ activeTab: tab });
-    }
-}
-// The initial state is added for the active tab and will start at 0 in the array of tabs I am creating. 
-// onClickTabItem will update the app state to the current tab that is clicked by the user. 
-
-// Now, I can add render function
-
-render() {
-    const {
-      onClickTabItem,
-      props: {
-        children,
-      },
-      state: {
-        activeTab,
-      }
-    } = this;
-
-    return (
-      <div className="tabs">
-        <ol className="tab-list">
-          {children.map((child) => {
-            const { label } = child.props;
-
-            return (
-              <Tab
-                activeTab={activeTab}
-                key={label}
-                label={label}
-                onClick={onClickTabItem}
-              />
-            );
-          })}
-        </ol>
-        <div className="tab-content">
-          {children.map((child) => {
-            if (child.props.label !== activeTab) return undefined;
-            return child.props.children;
-          })}
-        </div>
-      </div>
-    );
-  }
+const Tabs = () => {
+  return (
+    <div className="Tabs">
+      <p>Tab is working</p>
+    </div>
+  )
 }
 
 export default Tabs;
-
-    // the component keeps track of which tab is active, content for each tab, and displays a list of tabs. 
-    // the tabs component uses the next component you will create called "Tab"
