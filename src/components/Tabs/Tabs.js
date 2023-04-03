ca853ae592c0579c1d093ddc971334abe98b660a
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 19:37:22 -0700

    2 tab table done

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -8,16 +8,30 @@ const Tabs = () => {
   const [activeTab, setActiveTab] = useState("tabone");
 
 
+  // Functions to handle Tab switching 
+
+  const handleTabone = () => { //updates the state to tab one
+    setActiveTab("tabone");
+  }
+
+  const handleTabtwo = () => { // update the state to tab two 
+    setActiveTab("tabtwo");
+  };
+
+
+
   return (
     <div className="Tabs">
       <ul className="nav">
-        <li className={activeTab === "tabone" ? "active" : ""}>Tab one</li>
+
+        <li className={activeTab === "tabone" ? "active" : ""} onClick={handleTabone}>Tab 1</li>
         {/* tab one has a background color since it is the "active tab"*/}
-        <li className={activeTab === "tabtwo" ? "active" : ""}>Tab two</li>
+        <li className={activeTab === "tabtwo" ? "active" : ""} onClick={handleTabtwo}>Tab 2</li>
       </ul>
-      
+
       <div className="outlet">
-        {activeTab === "tabeone" ? <FirstTab /> : <SecondTab />}
+        {activeTab === "tabone" ? <FirstTab /> : <SecondTab />} 
+        {/* This tells react DOM to show the first tab when the active tab is tab one. otherwise it shows tab two  */}
       </div>
     </div>
   )
