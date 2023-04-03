d57eeb1929020b6cdaa063581545057ec9daeb39
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 18:34:11 -0700

    tabs component complete

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -3,7 +3,13 @@ import React from "react";
 const Tabs = () => {
   return (
     <div className="Tabs">
-      <p>Tab is working</p>
+      <ul className="tab-buttons">
+        <li>Tab one</li>
+        <li>Tab two</li>
+      </ul>
+      <div className="outlet">
+        <p>content will be shown here</p>
+      </div>
     </div>
   )
 }
