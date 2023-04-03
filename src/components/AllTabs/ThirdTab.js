d23caf54a3b68123efd7750fa2557f704bae9865
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 19:39:00 -0700

    third tab component created

---
+++ src/components/AllTabs/ThirdTab.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+
+const ThirdTab = () => {
+    return (
+        <div className="ThirdTab"> 
+        <p>third tab here</p>
+
+        </div>
+    )
+}
+
+export default ThirdTab; 
