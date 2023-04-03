7cb2148c66af5cb49d400c575956789f40cb9b6c
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 18:38:46 -0700

    first and second tab created

---
+++ src/components/AllTabs/FirstTab.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const FirstTab = () => {
+    return (
+        <div className="FirstTab">
+            <p>this is first tab</p>
+
+
+        </div>
+    )
+}
+
+export default FirstTab;
---
+++ src/components/AllTabs/SecTab.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const SecondTab = () => {
+    return (
+        <div className="SecondTab"> 
+        <p>second tab here</p>
+
+        </div>
+    )
+}
+
+export default SecondTab; 
