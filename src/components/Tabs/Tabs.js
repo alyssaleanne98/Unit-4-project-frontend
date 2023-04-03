a1675da9ca1b199799f07cd64ed7c147da28b450
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 19:20:15 -0700

    tabs component

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import FirstTab from "../AllTabs/FirstTab";
+import SecondTab from "../AllTabs/SecTab";
 import "./Tabs.css"
 
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("tabone");
+
+
   return (
     <div className="Tabs">
       <ul className="nav">
-        <li>Tab one</li>
-        <li>Tab two</li>
+        <li className={activeTab === "tabone" ? "active" : ""}>Tab one</li>
+        {/* tab one has a background color since it is the "active tab"*/}
+        <li className={activeTab === "tabtwo" ? "active" : ""}>Tab two</li>
       </ul>
+      
       <div className="outlet">
-        <p>content will be shown here</p>
+        {activeTab === "tabeone" ? <FirstTab /> : <SecondTab />}
       </div>
     </div>
   )
 }
+
 
 export default Tabs;
