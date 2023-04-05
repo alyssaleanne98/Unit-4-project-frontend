f971b907ceab635e0ff1e255794cd8b6c42d2558
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-04 23:37:14 -0700

    css added

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FirstTab from "../AllTabs/FirstTab";
 import SecondTab from "../AllTabs/SecTab";
+import ThirdTab from "../AllTabs/ThirdTab";
 import "./Tabs.css"
 
 
@@ -10,12 +11,16 @@ const Tabs = () => {
 
   // Functions to handle Tab switching 
 
-  const handleTabone = () => { //updates the state to tab one
+  const handleTabone = () => { // updates the state to tab one
     setActiveTab("tabone");
   }
 
   const handleTabtwo = () => { // update the state to tab two 
     setActiveTab("tabtwo");
+  };
+
+  const handleTabthree = () => { // update the state to tab two 
+    setActiveTab("tabthree");
   };
 
 
@@ -27,10 +32,11 @@ const Tabs = () => {
         <li className={activeTab === "tabone" ? "active" : ""} onClick={handleTabone}>Tab 1</li>
         {/* tab one has a background color since it is the "active tab"*/}
         <li className={activeTab === "tabtwo" ? "active" : ""} onClick={handleTabtwo}>Tab 2</li>
+        <li className={activeTab === "tabthree" ? "active" : ""} onClick={handleTabthree}>Tab 3</li>
       </ul>
 
       <div className="outlet">
-        {activeTab === "tabone" ? <FirstTab /> : <SecondTab />} 
+      {activeTab === "tabone" ? <FirstTab /> : <SecondTab />} 
         {/* This tells react DOM to show the first tab when the active tab is tab one. otherwise it shows tab two  */}
       </div>
     </div>
